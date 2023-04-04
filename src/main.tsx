487e558a4dab4eee59e09461c990abe9c9bc2044
Alisson de Oliveira <coding@alisson.dev>
2023-04-04 14:04:19 +0000

    ➕ Add React Query DevTools

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,14 @@ import App from "./App";
 import "./index.css";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { queryClient } from "./config/query-client";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <App />
+      {/* By default, will only be added on bundles with NODE_ENV === "development" */}
+      <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   </React.StrictMode>,
 );
